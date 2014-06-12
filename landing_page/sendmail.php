19d0c498ef4060ee4127220d8917f939166f6883
Eugene Batalov <eabatalov89@gmail.com>
2014-06-12 15:05:41 +0000

    landing: newsletter subscrive form implementation

--- landing_page/sendmail.php
+++ landing_page/sendmail.php
@@ -1,6 +1,13 @@
 <?php
 $EMAIL_FILE_PATH = '/idiom_land/emails.txt';
+$MB40 = 41943040; //40 Mb in bytes
 
-$emailData = sprintf("%s %s %s", $_POST['name'], $_POST['email'], PHP_EOL);
+/* Protect from DDOS */
+$currentFileSize = filesize($EMAIL_FILE_PATH);
+if ($currentFileSize !== FALSE && $currentFileSize > $MB40) {
+    exit("Error occurred!");
+}
+
+$emailData = sprintf("%s %s", $_POST['email'], PHP_EOL);
 file_put_contents($EMAIL_FILE_PATH, $emailData, FILE_APPEND);
 echo "Ok";
