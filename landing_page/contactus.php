d5d172c90697a0b5145d549582c5f03fe7598455
Eugene Batalov <eabatalov89@gmail.com>
2014-06-12 15:31:47 +0000

    landing: "contact us" form implementation

---
+++ landing_page/contactus.php
@@ -0,0 +1,17 @@
+<?php
+$CONTACTUS_FILE_PATH = '/idiom_land/contactus.txt';
+$MB40 = 41943040; //40 Mb in bytes
+
+/* Protect from DDOS */
+$currentFileSize = filesize($CONTACTUS_FILE_PATH);
+if ($currentFileSize !== FALSE && $currentFileSize > $MB40) {
+    exit("Error occurred!");
+}
+
+$contactData = sprintf("name: %s %semail: %s %smessage: %s %s",
+    $_POST['name'], PHP_EOL,
+    $_POST['email'], PHP_EOL,
+    $_POST['message'], PHP_EOL
+);
+file_put_contents($CONTACTUS_FILE_PATH, $contactData, FILE_APPEND);
+echo "Ok";
